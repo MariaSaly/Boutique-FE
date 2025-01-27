9ed74076bd2f7f8df2564fb10b2ab0895f94376b
RichdevosMuthusamy <richdevos.m@infonion.com>
2025-01-27 17:21:39 +0530

    search functionality is implemented

--- Boutique-FE/src/app/search-service.service.ts
+++ Boutique-FE/src/app/search-service.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { HttpService } from '../service/httpService';
+import { environment } from '../environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchServiceService {
+  public url = environment.localUrl;
 
-  constructor() { }
+  constructor(private http: HttpService) { }
 
-  search(query:string):Observable<any[]>{
-      console.log("query:",query);
-      const result:any = [];
-      return result
+  search(query: string): Observable<any[]> {
+    console.log("query:", query);
+    
+    // Ensure you're passing the query parameter to the API endpoint
+    return this.http.get<any[]>(`${this.url}/api/items/searchItems?query=${query}`);
   }
 }
