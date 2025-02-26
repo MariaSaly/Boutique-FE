0225a31449ae92f41d4f7702cbba5b0b288dce77
RichdevosMuthusamy <richdevos.m@infonion.com>
2025-02-26 15:55:01 +0530

    added new file

--- Boutique-FE/src/environment.ts
+++ Boutique-FE/src/environment.ts
@@ -2,14 +2,14 @@ export const environment = {
     production: false,
  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
  firebaseConfig : {
-    apiKey: "AIzaSyAF0eMfM7RFirRaP7CoTCQa7LmrLrPLI8k",
-    authDomain: "mokshedestination-boutique.firebaseapp.com",
-    projectId: "mokshedestination-boutique",
-    storageBucket: "mokshedestination-boutique.firebasestorage.app",
-    messagingSenderId: "520959613605",
-    appId: "1:520959613605:web:23b352de0dcb53b39c0b4c",
-    measurementId: "G-ELDR785DMQ"
-  },
+   apiKey: "AIzaSyCqlT6o7qvJkkIlCudSeI7n3CWmSOPkByE",
+   authDomain: "mokhedestination.firebaseapp.com",
+   projectId: "mokhedestination",
+   storageBucket: "mokhedestination.firebasestorage.app",
+   messagingSenderId: "345069403841",
+   appId: "1:345069403841:web:29b170233642b63064de40",
+   measurementId: "G-BF2025F8DM"
+ },
   localUrl:'http://localhost:5000'
   };
   
